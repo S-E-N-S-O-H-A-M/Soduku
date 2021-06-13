e775f74df89580f3ca2ec12c290ebbfd69e114f9
S-O-H-A-M <65547096+S-O-H-A-M@users.noreply.github.com>
2021-06-13 22:47:02 +0530

    Deleted comment

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ button.onclick = function() {
         changeBoard(board)
     }
     xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
-        //we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+       
     xhrRequest.send()
 }
 
